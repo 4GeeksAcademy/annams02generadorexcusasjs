dd6049cf4fea50e44168a179b9975a3aa99441f6
Annams02 <155559840+Annams02@users.noreply.github.com>
2024-02-11 20:19:01 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () =>	
+{
+	let who = new Array(); 
+		who[0] = "El perro";
+		who[1] = "El tigre";
+		who[2] = "El gato";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+	let action = new Array();				
+		action[0] = "se volvió rosa";
+		action[1] = "escapó por el mar";
+		action[2] = "me habló";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+	let when = new Array();		
+		when[0] = "hace 1 minuto";
+		when[1] = "justo ahora";
+		when[2] = "hace un rato";
+	{
+    document.querySelector("#message1").innerHTML = who
+    [Math.floor(Math.random()*who.length)]; 	
+    document.querySelector("#message2").innerHTML = action
+    [Math.floor(Math.random()*action.length)];	
+    document.querySelector("#message3").innerHTML = when
+    [Math.floor(Math.random()*when.length)]; 
+
+    }
+    var person = document.querySelector("#message1").innerHTML = who
+    [Math.floor(Math.random()*who.length)]; 
+    var accion1 = document.querySelector("#message2").innerHTML = accion
+    [Math.floor(Math.random()*accion.length)];
+    var tiempo1 = document.querySelector("#message3").innerHTML = when
+    [Math.floor(Math.random()*when.length)]; 
+    console.log(person);
+    console.log(action2);
+    console.log(when1);
+  
+  
+  
+  }
